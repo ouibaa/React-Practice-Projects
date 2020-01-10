4a1b6d65cf298968ed18f542bb0e9db4a3e74151
Weber Liu <wliu1996@hotmail.com.au>
2020-01-11 02:06:03 +1100

    Completed dog carousel application

--- carousel/src/App.js
+++ carousel/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function App() {
+  const [dogLinks, setDogLinks] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDogs = async () => {
+      const result = await fetch("https://dog.ceo/api/breeds/image/random/10");
+      const res = await result.json();
+      setDogLinks(res.message);
+      setLoading(false);
+    };
+
+    fetchDogs();
+  }, []);
+
+  // console.log(dogLinks); //checking to make sure the dogs show up
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="DogCarousel">
+      {loading ? (
+        <div>...Loading</div>
+      ) : (
+        <Carousel>
+          {dogLinks.map(dog => {
+            const dogData = dog.split("/")[4];
+
+            return (
+              <div key={dog}>
+                <img src={dog}></img>
+                <p className="legend">Breed: {dogData}</p>
+              </div>
+            );
+          })}
+        </Carousel>
+      )}
     </div>
   );
 }
